178d79f9a4eb3c93176f5038475f8454f523f652
SHIro1221 <labrada.rouco@gmail.com>
2025-06-18 18:34:35 -0400

    EDA 1 (cleaning data)
    I finished the cleaning data on the notebook

--- src/online_store.py
+++ src/online_store.py
@@ -1,7 +1,12 @@
 
 import pandas as pd
+import numpy as np
 
+df=pd.read_csv('dataset/raw/Sales Dataset.csv')
 
-df=pd.read_csv('dataset/Sales Dataset.csv')
-print(df.head())
+#cleaning data
+#drop column Unnamed: 0, because it is not useful 
+df.drop(columns=['Unnamed: 0'], inplace=True)
+#tranform the colummn Date to datetime
+df['Date']=pd.to_datetime(df['Date'])
 print(df.info())
